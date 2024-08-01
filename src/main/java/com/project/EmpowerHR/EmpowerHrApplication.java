a16c91684c30c87e0ad74f095cd3305c8258ce20
adem saidani <ademsaidani799@gmail.com>
2024-08-01 23:07:51 +0100

    first commit

---
+++ src/main/java/com/project/EmpowerHR/EmpowerHrApplication.java
@@ -0,0 +1,13 @@
+package com.project.EmpowerHR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpowerHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpowerHrApplication.class, args);
+	}
+
+}
